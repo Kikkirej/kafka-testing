26e4fa6ee0190d0ad80957d40734390ae1117533
Kikkirej <5462557+Kikkirej@users.noreply.github.com>
2023-01-11 21:42:33 +0100

    :tada: :construction: basic webservice and docker-compose added.

---
+++ webservice/src/main/kotlin/net/kikkirej/kafka/webservice/WebserviceApplication.kt
@@ -0,0 +1,11 @@
+package net.kikkirej.kafka.webservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebserviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebserviceApplication>(*args)
+}
